00ac4d7a9528dc38aa1ddd9f3a4f28a920bfa3d3
Daniel Jones <xXDJONESXx@hotmail.com>
2016-04-12 18:33:38 +1000

    Fixed PAR and PARC

--- PashLang/PASM/Handlers/set_PAR.cs
+++ PashLang/PASM/Handlers/set_PAR.cs
@@ -19,24 +19,33 @@ namespace PASM.Handlers
         int cpy_ptr;
 
         //Ranges we want to copy with
-        uint startingRange;
-        uint endingRange;
-
+        bool startingRange_isMethodPtr;
+        
+        int startingRange_Ptr;
+        
+        bool endingRange_isMethodPtr;
+        
+        int endingRange_Ptr;
+        
         public set_PAR(string[] args, Engine inst) : base(inst)
         {
             ts_isMethodPtr = isMethodPointer(args[1], out ts_ptr);
 
             cpy_isMethodPtr = isMethodPointer(args[3], out cpy_ptr);
 
-            startingRange = (uint)Converter.ParseStringToInt(args[4]);
-            endingRange = (uint)Converter.ParseStringToInt(args[5]);
+            startingRange_isMethodPtr = isMethodPointer(args[4], out startingRange_Ptr);
+            endingRange_isMethodPtr = isMethodPointer(args[5], out endingRange_Ptr);
 
         }
 
         public override void Execute()
         {
+            uint startingRange = (uint)inst.ResolveINT32(startingRange_isMethodPtr, startingRange_Ptr);
+            
+            uint endingRange = (uint)inst.ResolveINT32(endingRange_isMethodPtr, endingRange_Ptr);
+            
 			Raster.Register cpy_pointer = inst.GetRaster(cpy_isMethodPtr)[cpy_ptr];
-			inst.GetRaster(ts_isMethodPtr)[ts_ptr] = new Raster.Register(cpy_pointer.address + startingRange, endingRange);
+			inst.GetRaster(ts_isMethodPtr)[ts_ptr] = new Raster.Register(cpy_pointer.address + startingRange, startingRange + endingRange);
         }
     }
 }
